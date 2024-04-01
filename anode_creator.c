ebef9cedc9d355ed0431c9f2d491b514201b8379
Oussama Bajali <140127359+Sfx00@users.noreply.github.com>
2024-04-01 02:23:53 +0000

    ok

---
+++ anode_creator.c
@@ -0,0 +1,24 @@
+#include "monty.h"
+/**
+ * node_creator - add node to head stack.
+ * @head: head of stack.
+ * @n: new_value.
+ *
+*/
+void anode_creator(stack_t **head, int n)
+{
+
+	stack_t *new_nodee, *temp_heade;
+
+	temp_heade = *head;
+	new_nodee = malloc(sizeof(stack_t));
+	if (new_nodee == NULL)
+	{ printf("Error\n");
+		exit(0); }
+	if (temp_heade)
+		temp_heade->prev = new_nodee;
+	new_nodee->n = n;
+	new_nodee->next = *head;
+	new_nodee->prev = NULL;
+	*head = new_nodee;
+}
