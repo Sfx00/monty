94ca492995f8b5ba908972ab8a84da213898ff16
Oussama Bajali <140127359+Sfx00@users.noreply.github.com>
2024-04-01 02:12:54 +0000

    ok

--- main.c
+++ main.c
@@ -1,45 +1,45 @@
 #include "monty.h"
 pay_data pusher = {NULL, NULL, NULL, 0};
 /**
- * main - monty code interpreter
- * @argc: arguments count
- * @argv: program name
- * Return: 0 on success
+ * main - monty code interpreter.
+ * @argc: arguments count.
+ * @argv: program name.
+ * Return: 0 on success.
  */
 int main(int argc, char *argv[])
 {
-	char *content;
-	FILE *file;
-	size_t size = 0;
-	ssize_t read_line = 1;
-	stack_t *stack = NULL;
-	unsigned int line_counter = 0;
+	char *contente;
+	FILE *filee;
+	size_t sizee = 0;
+	ssize_t read_linee = 1;
+	stack_t *stacke = NULL;
+	unsigned int line_countere = 0;
 
 	if (argc != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
-	file = fopen(argv[1], "r");
-	pusher.file = file;
-	if (!file)
+	filee = fopen(argv[1], "r");
+	pusher.file = filee;
+	if (!filee)
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
-	while (read_line > 0)
+	while (read_linee > 0)
 	{
-		content = NULL;
-		read_line = getline(&content, &size, file);
-		pusher.content = content;
-		line_counter++;
-		if (read_line > 0)
+		contente = NULL;
+		read_linee = getline(&contente, &sizee, filee);
+		pusher.content = contente;
+		line_countere++;
+		if (read_linee > 0)
 		{
-			action(content, &stack, line_counter, file);
+			action(contente, &stacke, line_countere, filee);
 		}
-		free(content);
+		free(contente);
 	}
-	free_sll(stack);
-	fclose(file);
+	free_sll(stacke);
+	fclose(filee);
 	return (0);
 }
